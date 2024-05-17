23df42abf8137532bb85aa226c7b0854d5c9596d
smishra677 <73450843+smishra677@users.noreply.github.com>
2024-05-17 15:49:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-  name='reconcILS',
-  version='0.1.0',
-  description='reconcILS',
-  author='N',
-  author_email='samishr@iu.edu',
-  url='https://github.com/smishra677/reconcILS/',
-  packages=['reconcILS'],
-  install_requires=[
-    'pandas',
-    'matplotlib',
-    'uuid',
-    'igraph',
-    
-  ],
+    name='reconcILS',
+    version='0.1.0',
+    description='A package for gene tree-species tree reconciliation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Sarthak Mishra',
+    author_email='samishr@iu.edu',
+    packages=find_packages(),
+    install_requires=[
+      'pandas',
+      'matplotlib',
+      'uuid',
+      'igraph',
+      
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
 
